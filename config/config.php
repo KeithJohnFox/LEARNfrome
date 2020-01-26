b00b2bb16551b78456e352298de9b8f80f515256
Keith Fox <43954826+KeithJohnFox@users.noreply.github.com>
2020-01-26 18:12:57 +0000

    new

--- config/config.php
+++ config/config.php
@@ -1,13 +1,13 @@
 <?php
-    ob_start(); //this turns on output bufering, passing php code at the end of the browser when it loads to speed the loading time  
+    ob_start(); //this turns on output bufering, passing php code at the end of the browser when it loads to speed the loading time
     session_start();    //Session start stores values of the variables inside the session variable
-    $con = mysqli_connect("localhost", "root", "", "mydatabase");
+    $con = mysqli_connect("localhost", "root", "", "learnfrome");
 
     $timezone = date_default_timezone_set("Europe/Dublin"); //sets the default time to dublin
 
     // if statement is used if there is an error by using the errno command AND the . is append allowing you to join 2 strings
-    if(mysqli_connect_errno()) 
+    if(mysqli_connect_errno())
     {
-        echo "Failed to connect: " . mysqli_connect_errno();    
+        echo "Failed to connect: " . mysqli_connect_errno();
     }
 ?>
